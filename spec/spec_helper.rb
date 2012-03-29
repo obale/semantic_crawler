d6fef5ac648a5f56c55581f5c1f4cc05539a8a55
Alex Oberhauser <alex.oberhauser@sigimera.org>
2012-03-29 22:20:57 +0200

    Fixing rcov output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,12 @@
-require 'rubygems'
-require 'bundler/setup'
-
-require 'semantic_crawler'
-
 require 'simplecov'
 require 'simplecov-rcov'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start 'rails'
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'semantic_crawler'
 
 RSpec.configure do |config|
 end
